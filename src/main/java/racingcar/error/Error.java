2e355efd8ada6a8bbf5d3d18781f24f86c5e0be1
leebongho <leebongho9204@gmail.com>
2022-04-22 11:15:21 +0900

    feat: 쉼표 구분자로 분리하여 문자열을 저장하는 LineUp 구현
    추가 내용
    - 문자열을 입력받아 쉼표 구분자로 분리하여 String 배열에 저장
    - 다음 순서가 존재하는지에 대한 여부를 반환하는 hasNext 구현
    - 다음 순서를 반환하는 next 구현

--- src/main/java/racingcar/error/Error.java
+++ src/main/java/racingcar/error/Error.java
@@ -2,7 +2,8 @@ package racingcar.error;
 
 public enum Error {
     NOT_VALID_CAR_NAME("[ERROR] 유효하지 않은 이름입니다."),
-    NOT_VALID_LEVEL("[ERROR] Rpm Level은 0 ~ 9사이의 숫자만 가능합니다.");
+    NOT_VALID_LEVEL("[ERROR] Rpm Level은 0 ~ 9사이의 숫자만 가능합니다."),
+    HAS_NOT_NEXT("[ERROR] 모든 LineUp이 배정되었습니다.");
 
     private final String message;
     Error(String message) {
---
+++ src/test/java/racingcar/model/racingvehicle/wrapper/LineUpTest.java
@@ -0,0 +1,61 @@
+package racingcar.model.racingvehicle.wrapper;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.error.Error;
+
+public class LineUpTest {
+
+    private LineUp lineUp;
+
+    @Test
+    @DisplayName("lee,kim,ho에 대해 hasNext 메서드 true 반환")
+    void 쉼표_기준_hasNext_여부_반환() {
+        String lineup = "lee,kim,ho";
+        lineUp = new LineUp(lineup);
+
+        assertThat(lineUp.hasNext()).isTrue();
+    }
+
+    @Test
+    @DisplayName("next 메서드에 대해 쉼표 기준 분리된 이름 반환")
+    void 쉼표_기준_분리된_이름_반환_테스트() {
+        String lineup = "lee,kim,ho";
+        lineUp = new LineUp(lineup);
+
+        assertThat(lineUp.next()).isEqualTo("lee");
+        assertThat(lineUp.next()).isEqualTo("kim");
+        assertThat(lineUp.next()).isEqualTo("ho");
+    }
+
+    @Test
+    @DisplayName("모든 LineUp 배정 시 hasNext false 반환")
+    void LineUp_크기만큼_추출시_hasNext_false_반환() {
+        String lineup = "lee,kim,ho";
+        lineUp = new LineUp(lineup);
+        extractFromLineup(lineup.split(",").length);
+
+        assertThat(lineUp.hasNext()).isFalse();
+    }
+
+    @Test
+    @DisplayName("다음 LineUp 없을 시 IllegalStatementException 반환")
+    void 모든_LineUp_소진이후_추출시_예외발생() {
+        String lineup = "lee,kim,ho";
+        lineUp = new LineUp(lineup);
+        extractFromLineup(lineup.split(",").length);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> lineUp.next());
+        assertThat(e.getMessage()).isEqualTo(Error.HAS_NOT_NEXT.message());
+    }
+
+    private void extractFromLineup(int count) {
+        for (int i = 0; i < count; i++) {
+            lineUp.next();
+        }
+    }
+}
