a2e6490ef6146051a04b7492f0d2a5a5807b6daa
bongholee <leebongho9204@gmail.com>
2022-04-24 22:07:39 +0900

    refactor: 최종 우승자 출력 변수 명 변경

--- src/main/java/racingcar/view/Display.java
+++ src/main/java/racingcar/view/Display.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import racingcar.model.record.Records;
 
 public class Display {
-    private static final String FIRST_PLACE = "최종 우승자: ";
+    private static final String WINNERS = "최종 우승자: ";
     private static final String LINEUP_MESSAGE = "경주 할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private static final String SET_LAPS_MESSAGE = "시도할 Laps 수를 입력하세요.";
 
@@ -20,7 +20,7 @@ public class Display {
     }
 
     public static void printFinalRank(Records records) {
-        System.out.println(FIRST_PLACE + records.rankSheet());
+        System.out.println(WINNERS + records.rankSheet());
     }
 
     public static void printExceptionMessage(RuntimeException e) {
