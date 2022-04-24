a6984c2c6eb6970c932570cd1f91e56d43e72bf4
bongholee <leebongho9204@gmail.com>
2022-04-24 21:32:48 +0900

    test: 자동차 전진 횟수에 따른 최종 순위 출력 테스트코드 작성

--- src/test/java/racingcar/model/record/RecordsTest.java
+++ src/test/java/racingcar/model/record/RecordsTest.java
@@ -1,5 +1,8 @@
 package racingcar.model.record;
 
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.car.distance.ForwardDistance;
 import racingcar.model.car.name.CarName;
@@ -10,9 +13,11 @@ public class RecordsTest {
     private Records records = new Records();
 
     @Test
-    void test() {
+    @DisplayName("거리가 3, 1, 2일 때 3이 ranker")
+    void 거리가_3_1_2_일때_3이_ranker_test() {
         records.addRecord(new Record(new CarName("lee"), new ForwardDistance(3)));
         records.addRecord(new Record(new CarName("kim"), new ForwardDistance(1)));
         records.addRecord(new Record(new CarName("ho"), new ForwardDistance(2)));
+        Assertions.assertThat(records.rankSheet()).isEqualTo("lee");
     }
 }
